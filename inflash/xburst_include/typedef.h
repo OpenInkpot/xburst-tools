5a429fc447f2b04bdae18bde31de4e5a227dd99e
xiangfu <xiangfu.z@gmail.com>
2009-06-26 00:16:04 +0000

    add typedef.h

---
+++ inflash/xburst_include/typedef.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2009 PI
+ * Author:  Xiangfu Liu <xiangfu.z@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+#ifndef __TYPEDEF_H__
+#define __TYPEDEF_H__
+
+typedef unsigned int size_t;
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+#endif /* __TYPEDEF_H__ */
