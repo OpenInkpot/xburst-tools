f426319446b5834df4715d07c23a099cd7678c3d
Xiangfu Liu <xiangfu.z@gmail.com>
2009-07-21 01:12:38 +0800

    change the default usbboot.cfg path
    Signed-off-by: Xiangfu Liu <xiangfu.z@gmail.com>

--- usbboot/src/main.c
+++ usbboot/src/main.c
@@ -26,7 +26,7 @@
 #include "ingenic_usb.h"
 #include "ingenic_cfg.h"
 
-#define CONFIG_FILE_PATH "/usr/share/xburst-tools/usbboot.cfg"
+#define CONFIG_FILE_PATH "/etc/xburst-tools/usbboot.cfg"
 
 extern struct ingenic_dev ingenic_dev;
 extern struct hand hand;
