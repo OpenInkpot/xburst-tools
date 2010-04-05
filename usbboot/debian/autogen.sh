bf43104a6a8b43a09ffda57bee6c7c97f9239340
Jonathan Nieder <jrnieder@gmail.com>
2010-04-05 09:23:20 -0500

    xburst-tools: generate changelog from git log
    Use git shortlog to generate a brief list of changes to put in
    /usr/share/doc/xburst-tools/changelog.gz.  This list is built
    at source package generation time because it requires access to
    the git repository.  The commit ID corresponding to each upstream
    release is extracted from debian/changelog since the repository
    has no tags.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>

--- usbboot/debian/autogen.sh
+++ usbboot/debian/autogen.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-# Generate debian/xburst_stage1.bin and debian/xburst_stage2.bin
+# Generate debian/xburst_stage1.bin, debian/xburst_stage2.bin,
+# and debian/changelog.upstream.
+#
+# Uses debian/changelog and the git revision log.
 #
 # Requires a mipsel-openwrt-linux- toolchain on the $PATH.
 
 set -e
+
+dpkg-parsechangelog --format rfc822 --all |
+	awk -f debian/changelog.upstream.awk
 
 debian/rules firmware
 cp -f xburst_stage1/xburst_stage1.bin debian/
