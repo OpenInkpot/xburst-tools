41ee624696291d339d2bb2b9b306ab15d44be7e4
Xiangfu Liu <xiangfu@sharism.cc>
2010-06-09 15:20:02 +0800

    [xbboot] rename the 4740 function suffix with _4740

--- xbboot/target-stage1/stage1.c
+++ xbboot/target-stage1/stage1.c
@@ -16,7 +16,13 @@ extern void serial_init_4740(int uart);
 extern void sdram_init_4740();
 extern void nand_init_4740();
 
-void load_args()
+extern void gpio_init_4760();
+extern void pll_init_4760();
+extern void cpm_start_all();
+extern void serial_init_4760(int uart);
+extern void sdram_init_4760();
+
+void load_args_4740()
 {
 	ARG_CPU_ID = 0x4740;
 	ARG_EXTAL = 12 * 1000000;
@@ -29,9 +35,23 @@ void load_args()
 	ARG_COL_ADDR = 9;
 }
 
+void load_args_4760()
+{
+	ARG_CPU_ID = 0x4760;
+	ARG_EXTAL = 12 * 1000000;
+	ARG_CPU_SPEED = 2 * ARG_EXTAL;
+	ARG_PHM_DIV = 3;
+	ARG_UART_BAUD = 57600;
+	ARG_BUS_WIDTH_16 = * (int *)0x80002014;
+	ARG_BANK_ADDR_2BIT = 1;
+	ARG_ROW_ADDR = 12;
+	ARG_COL_ADDR = 9;
+}
+
+
 void c_main(void)
 {
-	load_args();
+	load_args_4740();
 
 	switch (ARG_CPU_ID)	{
 	case 0x4740:
